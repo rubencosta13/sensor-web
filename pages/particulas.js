fa5ec89f51d942b75b27ba2c3a89ef51d4f5c438
Rúben Costa <rubenlavoscosta@gmail.com>
2022-04-30 11:44:16 +0100

    Fixed React18's error Hydration

--- pages/particulas.js
+++ pages/particulas.js
@@ -2,17 +2,17 @@ const Particulas = () => {
   return (
     <div className="container">
         <h2 className="text-center">As Particulas</h2>
-        <p>As PM (Particulas Materiais) podem ser classificadas quanto à dimensão do seu diâmetro aerodinâmico equivalente, termo usado para definir as propriedades aerodinâmicas das partículas, é definido como sendo o diâmetro de uma esfera equivalente</p>
+        <div>As PM (Particulas Materiais) podem ser classificadas quanto à dimensão do seu diâmetro aerodinâmico equivalente, termo usado para definir as propriedades aerodinâmicas das partículas, é definido como sendo o diâmetro de uma esfera equivalente</div>
         <div className="mb-4">
             <h3 className="text-center">Particulas PM 10</h3>
-            <p>(PM10) partículas de pequenas dimensões com um diâmetro aerodinâmico equivalente inferior a 10 micrómetros De forma a garantir a proteção das pessoas à exposição deste poluente, foi estabelecido o valor limite (VL) diário de PM10 (50 μg/m3, que não deve ser excedido mais de 35 dias por ano civil) e do VL anual (40 μg/m3). </p>
+            <div>(PM10) partículas de pequenas dimensões com um diâmetro aerodinâmico equivalente inferior a 10 micrómetros De forma a garantir a proteção das pessoas à exposição deste poluente, foi estabelecido o valor limite (VL) diário de PM10 (50 μg/m3, que não deve ser excedido mais de 35 dias por ano civil) e do VL anual (40 μg/m3). </div>
             
-            <p>São utilizados dois indicadores para avaliar a exposição às partículas PM10: o número de excedências ao valor limite diário e o valor limite da média anual, e sempre que os níveis medidos se encontrem acima do VL devem ser implementadas medidas, no mais curto espaço de tempo, para garantir que a população não esteja exposta a níveis que representem riscos significativos para a saúde</p>
+            <div>São utilizados dois indicadores para avaliar a exposição às partículas PM10: o número de excedências ao valor limite diário e o valor limite da média anual, e sempre que os níveis medidos se encontrem acima do VL devem ser implementadas medidas, no mais curto espaço de tempo, para garantir que a população não esteja exposta a níveis que representem riscos significativos para a saúde</div>
             <h3 className="text-center">Particulas PM 2.5</h3>
-            <p>(PM2.5)  partículas de  pequenas dimensões com um diâmetro aerodinâmico equivalente inferior a 2.5 micrómetro são geradas principalmente pela queima de combustível fóssil por veículos e pela queima de 21 biomassa por indústrias.</p>
+            <div>(PM2.5)  partículas de  pequenas dimensões com um diâmetro aerodinâmico equivalente inferior a 2.5 micrómetro são geradas principalmente pela queima de combustível fóssil por veículos e pela queima de 21 biomassa por indústrias.</div>
             <hr/>
         </div>
-        <p>Na tabela que se segue, são apresentados os valores para a classificação das particulas</p>
+        <div>Na tabela que se segue, são apresentados os valores para a classificação das particulas</div>
         <div className="row justify-content-center"> 
             <div className="table-responsive col-auto" style={{"width": "60%"}} >
                 <table className="table p-3 mb-2">
