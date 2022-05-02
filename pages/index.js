fc760fffaded8a2e913882f1997937937f48bd8c
Rúben Costa <rubenlavoscosta@gmail.com>
2022-05-02 22:51:22 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -43,8 +43,6 @@ const Home = () => {
       <div style={{width:'100%', height:'80vh'}} id="chart" className="mb-9">
       {(isBrowser) ? <ChartViewer/> : (orientation === "landscape-secondary" || orientation === "landscape-primary") ? <ChartViewer/>: <div className="d-flex justify-content-center">Rode o seu dispositivo e atualize a página para visualizar o gráfico<br/><button className="btn btn-danger" onClick={() => router.reload(window.location.pathname)}>Atualizar a página</button></div>}
       </div>
-      
-
     </div>
   );
 };
