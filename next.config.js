5aa7ad63d2bfa1876242a6a7293eae9a074900ba
Rúben Costa <rubenlavoscosta@gmail.com>
2022-05-05 19:54:47 +0100

    Optimized CSS bundles

--- next.config.js
+++ next.config.js
@@ -1,4 +1,70 @@
-module.exports = {
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
+
+const path = require('path')
+const glob = require('glob')
+const webpack = require('webpack')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const PurgecssPlugin = require('purgecss-webpack-plugin')
+
+const outputDir = './wwwroot/dist'
+const entry = './wwwroot/js/app.js'
+const cssOutput = 'site.css'
+
+const withCss = require("@zeit/next-css");
+const withPurgeCss = require("next-purgecss");
+
+
+module.exports = (env) => {    
+  return [{
+      entry: entry,
+      output: {
+          path: path.join(__dirname, outputDir),
+          filename: '[name].js',
+          publicPath: '/dist/'
+      },
+      module: {
+          rules: [
+              {
+                  test: /\.js$/,
+                  use: 'babel-loader'
+              },
+              {
+                  test: /\.css$/,
+                  use: ExtractTextPlugin.extract({
+                      use: ['css-loader'],
+                      fallback: 'style-loader'
+                  })
+              },
+              {
+                  test: /\.scss$/,
+                  use: ExtractTextPlugin.extract({
+                      use: ['css-loader', 'sass-loader' ],
+                      fallback: 'style-loader'
+                  })
+              }
+          ]
+      },
+      plugins: [
+          new ExtractTextPlugin(cssOutput),
+          new PurgecssPlugin({
+              paths: glob.sync('./Views/**/*.cshtml', { nodir: true }),
+              whitelistPatterns: [ /selectize-.*/ ]
+          })
+      ]
+  }]
+}
+
+module.exports = withCss(withPurgeCss({
+  purgeCssPaths: [
+    "pages/**/*",
+    "components/**/*",
+  ],
+  purgeCssEnabled: ({ dev, isServer }) => !dev && !isServer, // Only enable PurgeCSS for client-side production builds
+}));
+
+module.exports = withBundleAnalyzer({
     webpackDevMiddleware: (config) => {
       config.watchOptions.poll = 300;
       return config;
@@ -32,4 +98,4 @@ module.exports = {
         }
       ],
     ]
-};
+});
