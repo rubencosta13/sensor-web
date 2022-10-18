57d0151025bbd0516837cabccebbd8f34bade1af
Rúben Costa <66113929+rubencosta13@users.noreply.github.com>
2022-10-18 14:08:35 +0100

    updates

--- components/chart.tsx
+++ components/chart.tsx
@@ -17,6 +17,9 @@ const getData = async (setCss) => {
     yesterday = Math.floor(yesterday.valueOf()/1000)
     try {
         const sensorData = await axios.post(`https://h2801469.stratoserver.net/get.php?id=2475238&from=${yesterday}&to=${currentDate}&minimize=false&with_gps=true&with_note=true`)
+        console.groupCollapsed('> Raw data');
+        console.log(sensorData.data);
+        console.groupEnd();
         if (sensorData.data.length = 0) {
             console.group();
             console.log('Data: ');
