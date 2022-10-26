564ad42b7132dfd79f2b17d3bf684625e1864a28
Rúben Costa <rubenlavoscosta@gmail.com>
2022-10-26 16:20:42 +0100

    bug fixes

--- next.config.js
+++ next.config.js
@@ -15,8 +15,15 @@ const cssOutput = 'site.css'
 const withCss = require("@zeit/next-css");
 const withPurgeCss = require("next-purgecss");
 
+module.exports = {
+  compiler: {
+    styledComponents: true | {
+      minify: true
+    },
+  }
+};
 
-module.exports = (env) => {    
+module.exports = (env) => { 
   return [{
       entry: entry,
       output: {
@@ -65,37 +72,9 @@ module.exports = withCss(withPurgeCss({
 }));
 
 module.exports = withBundleAnalyzer({
-    webpackDevMiddleware: (config) => {
-      config.watchOptions.poll = 300;
-      return config;
-    },
-    images: {
-      domains: ['i.imgur.com', 'sensor.community'],
-    },
-    "plugins": [
-      "postcss-flexbugs-fixes",
-      [
-        "postcss-preset-env",
-        {
-          "autoprefixer": {
-            "flexbox": "no-2009"
-          },
-          "stage": 3,
-          "features": {
-            "custom-properties": false
-          }
-        }
-      ],
-      [
-        '@fullhuman/postcss-purgecss',
-        {
-          content: [
-              './pages/**/*.{js,jsx,ts,tsx}',
-              './components/**/*.{js,jsx,ts,tsx}'
-          ],
-          defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
-          safelist: ["html", "body"]
-        }
-      ],
-    ]
+  images: {
+    domains: ['i.imgur.com', 'sensor.community'],
+  },
+  swcMinify: true, 
+  optimizeFonts: true,
 });
