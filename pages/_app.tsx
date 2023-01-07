238bcfd38b18bbc57925b94063de37d6e9850405
Rúben Costa <rubenlavoscosta@gmail.com>
2023-01-07 22:37:01 +0000

    Add A/B Testing with google optimize

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,46 +5,74 @@ import 'bootstrap/dist/css/bootstrap.css';
 import GoogleAnalytics from '@bradgarropy/next-google-analytics';
 
 const MyApp = ({ Component, pageProps }: any) => {
-	return (
-		<>
-			<Head>
-				<meta property="og:title" content="Qualidade do ar na Marinha Grande" />
-				<meta
-					property="og:description"
-					content="Plataforma de monitorização da qualidade do ar na Marinha Grande, Leiria. Sensor de monitorização da escola Secundária Pinhal do Rei"
-				/>
-				<meta name="title" content="Qualidade do ar na Marinha Grande" />
-				<meta
-					name="description"
-					content="Plataforma de monitorização da qualidade do ar na Marinha Grande, Leiria. Sensor de monitorização da escola Secundária Pinhal do Rei"
-				/>
-				<meta
-					name="keywords"
-					content="Qualidade do Ar, Marinha Grande, IPMA, leiria, sensor de particulas, ESPR, pinhal do rei"
-				/>
-				<meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
-				<meta name="language" content="Portuguese" />
-				<meta name="revisit-after" content="2 days" />
-				<meta name="author" content="Ruben Lavos Costa" />
-				<meta name="yandex-verification" content="75168f966d0e5eee" />
-				<link
-					rel="shortcut icon"
-					href="https://www.cm-mgrande.pt/assets/cmmgrande/cmmgrande/images/favicon.ico"
-				/>
-				<title>Qualidade do ar na Marinha Grande</title>
-				<meta
-					name="google-site-verification"
-					content="01GcahiJ-eCHp7RzZLZ8M_GXL_bg1jSX9oFgouFGpUA"
-				/>
-				<meta name="viewport" content="initial-scale=1.0, width=device-width" />
-			</Head>
-			<Layout>
-				<div></div>
-				<GoogleAnalytics measurementId="G-KC26BBGRR9" />
-				<Component {...pageProps} />
-			</Layout>
-		</>
-	);
+  return (
+    <>
+      <Head>
+        <meta
+          property="og:title"
+          content="Qualidade do ar na Marinha Grande"
+        />
+        <meta
+          property="og:description"
+          content="Plataforma de monitorização da qualidade do ar na Marinha Grande, Leiria. Sensor de monitorização da escola Secundária Pinhal do Rei"
+        />
+        <meta
+          name="title"
+          content="Qualidade do ar na Marinha Grande"
+        />
+        <meta
+          name="description"
+          content="Plataforma de monitorização da qualidade do ar na Marinha Grande, Leiria. Sensor de monitorização da escola Secundária Pinhal do Rei"
+        />
+        <meta
+          name="keywords"
+          content="Qualidade do Ar, Marinha Grande, IPMA, leiria, sensor de particulas, ESPR, pinhal do rei"
+        />
+        <meta
+          httpEquiv="Content-Type"
+          content="text/html; charset=utf-8"
+        />
+        <meta
+          name="language"
+          content="Portuguese"
+        />
+        <meta
+          name="revisit-after"
+          content="2 days"
+        />
+        <meta
+          name="author"
+          content="Ruben Lavos Costa"
+        />
+        <meta
+          name="yandex-verification"
+          content="75168f966d0e5eee"
+        />
+        <link
+          rel="shortcut icon"
+          href="https://www.cm-mgrande.pt/assets/cmmgrande/cmmgrande/images/favicon.ico"
+        />
+        <title>Qualidade do ar na Marinha Grande</title>
+        <script
+          async
+          src="https://www.googleoptimize.com/optimize.js?id=OPT-NNG4M4B"
+        ></script>
+        <meta
+          name="google-site-verification"
+          content="01GcahiJ-eCHp7RzZLZ8M_GXL_bg1jSX9oFgouFGpUA"
+        />
+        <meta
+          name="viewport"
+          content="initial-scale=1.0, width=device-width"
+        />
+      </Head>
+      <Layout>
+        <div></div>
+        <GoogleAnalytics measurementId="G-KC26BBGRR9" />
+        <Component {...pageProps} />
+      </Layout>
+    </>
+  );
 };
 
 export default MyApp;
